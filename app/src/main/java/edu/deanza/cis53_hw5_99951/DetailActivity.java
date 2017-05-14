be051c452f14497c1f32f567e17df563cc7d2b63
tomerlich <tomer.erlich2@gmail.com>
2017-05-14 15:57:19 -0700

    added the detail activity and the xml for this activity

---
+++ app/src/main/java/edu/deanza/cis53_hw5_99951/DetailActivity.java
@@ -0,0 +1,17 @@
+package edu.deanza.cis53_hw5_99951;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tomer on 5/14/2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
