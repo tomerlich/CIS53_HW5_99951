d55e3a49b8c899182d39123da506ae7283a4c9f1
tomerlich <tomer.erlich2@gmail.com>
2017-05-22 12:28:08 -0700

    edited teh test code

--- app/src/androidTest/java/edu/deanza/cis53_hw5_99951/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/deanza/cis53_hw5_99951/ExampleInstrumentedTest.java
@@ -1,11 +1,15 @@
 package edu.deanza.cis53_hw5_99951;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import edu.deanza.cis53_hw5_99951.data.WeatherDbHelper;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +20,23 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    public static final String APP_TAG = "CIS53_HW6_99951";
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("edu.deanza.cis53_hw5_99951", appContext.getPackageName());
+    }
+
+    @Test
+    public void testCreateDb() throws Throwable{
+        Log.d(APP_TAG, "testCreateDb");
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        appContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
+        SQLiteDatabase db = new WeatherDbHelper(
+                appContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
+        db.close();
     }
 }
