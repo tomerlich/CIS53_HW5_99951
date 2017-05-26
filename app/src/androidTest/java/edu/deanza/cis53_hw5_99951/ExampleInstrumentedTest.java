7bd11aef42634672c0ee9bc4c218ebb86fbb4fba
tomerlich <tomer.erlich2@gmail.com>
2017-05-25 18:49:45 -0700

    the code for assignment 6 is now complete

--- app/src/androidTest/java/edu/deanza/cis53_hw5_99951/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/deanza/cis53_hw5_99951/ExampleInstrumentedTest.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Set;
 
 import edu.deanza.cis53_hw5_99951.data.WeatherContract;
+import edu.deanza.cis53_hw5_99951.data.WeatherContract.*;
 import edu.deanza.cis53_hw5_99951.data.WeatherDbHelper;
 
 import static org.junit.Assert.*;
@@ -46,9 +47,50 @@ public class ExampleInstrumentedTest {
         db.close();
     }
 
+    static ContentValues createWeatherValues(long locationRowId){
+        ContentValues weatherValues = new ContentValues();
+        weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        weatherValues.put(WeatherEntry.COLUMN_DATETEXT, "20141205");
+        weatherValues.put(WeatherEntry.COLUMN_DEGREES, 1.1);
+        weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 1.2);
+        weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 1.3);
+        weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 75);
+        weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 65);
+        weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+        weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+        weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 321);
+
+        return weatherValues;
+    }
+
+    static ContentValues createNorthPoleLocationValues(){
+        ContentValues testValues = new ContentValues();
+        testValues.put(LocationEntry.COLUMN_LOCATION_SETTING, "99705");
+        testValues.put(LocationEntry.COLUMN_CITY_NAME, "North Pole");
+        testValues.put(LocationEntry.COLUMN_COORD_LAT, 64.7488);
+        testValues.put(LocationEntry.COLUMN_COORD_LONG, -147.353);
+
+        return testValues;
+    }
+
+    static void validateCursor(Cursor valueCursor, ContentValues expectedValues){
+
+        assertTrue(valueCursor.moveToFirst());
+
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet){
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse(idx == -1);
+            String expectedValue = entry.getValue().toString();
+            assertEquals(expectedValue, valueCursor.getString(idx));
+        }
+        valueCursor.close();
+    }
+
     @Test
     public void testInsertReadDb(){
-        Log.d(APP_TAG, "testInsertReadDb");
+        Log.d(APP_TAG, "testInsertReadDb ");
         Context appContext = InstrumentationRegistry.getTargetContext();
         // If there's an error in those massive SQL table creation Strings,
         // errors will be thrown here when you try to get a writable database.
@@ -69,7 +111,7 @@ public class ExampleInstrumentedTest {
 
         // A cursor is your primary interface to the query results;
         Cursor cursor = db.query(
-                WeatherContract.LocationEntry.TABLE_NAME, // Table to Query
+                LocationEntry.TABLE_NAME, // Table to Query
                 null,   // all columns
                 null,   // columns for the "where" clause
                 null,   // Values for the "where" clause
@@ -83,12 +125,12 @@ public class ExampleInstrumentedTest {
         //Fantastic Now that we have a location, add some weather!
         ContentValues weatherValues = createWeatherValues(locationRowId);
 
-        long weatherRowId = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, weatherValues);
+        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherValues);
         assertTrue(weatherRowId != -1);
 
         // A cursor is your primary interface to the query results
         Cursor weatherCursor = db.query(
-                WeatherContract.WeatherEntry.TABLE_NAME, // Table to Query
+                WeatherEntry.TABLE_NAME, // Table to Query
                 null,   // all columns
                 null,   // columns for the "where" clause
                 null,   // Values for the "where" clause
@@ -100,46 +142,5 @@ public class ExampleInstrumentedTest {
         validateCursor(weatherCursor, weatherValues);
 
         dbHelper.close();
-    }
-
-    static ContentValues createWeatherValues(long locationRowId){
-        ContentValues weatherValues = new ContentValues();
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_LOC_KEY, locationRowId);
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DATETEXT, "20141205");
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_DEGREES, 1.1);
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 1.2);
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1.3);
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP, 75);
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP, 65);
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5.5);
-        weatherValues.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_ID, 321);
-
-        return weatherValues;
-    }
-
-    static ContentValues createNorthPoleLocationValues(){
-        ContentValues testValues = new ContentValues();
-        testValues.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING, "99705");
-        testValues.put(WeatherContract.LocationEntry.COLUMN_CITY_NAME, "North Pole");
-        testValues.put(WeatherContract.LocationEntry.COLUMN_COORD_LAT, 64.7488);
-        testValues.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING, -147.353);
-
-        return testValues;
-    }
-
-    static void validateCursor(Cursor valueCursor, ContentValues expectedValues){
-
-        assertTrue(valueCursor.moveToFirst());
-
-        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
-        for (Map.Entry<String, Object> entry : valueSet){
-            String columnName = entry.getKey();
-            int idx = valueCursor.getColumnIndex(columnName);
-            assertFalse(idx == -1);
-            String expectedValue = entry.getValue().toString();
-            assertEquals(expectedValue, valueCursor.getString(idx));
-        }
-        valueCursor.close();
     }
 }
